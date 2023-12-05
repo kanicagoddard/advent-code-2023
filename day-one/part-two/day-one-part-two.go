8ac53695cf4700325ea94ef002ebcf95039aa19b
kanica.goddard <kanicagoddard@gmail.com>
2023-12-05 11:10:55 -0500

    remove unused value

--- day-one/part-two/day-one-part-two.go
+++ day-one/part-two/day-one-part-two.go
@@ -22,7 +22,7 @@ import (
 
 // map of spelled out numbers to their digit form
 var numberMap = map[string]int{
-	"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
+	"one": 1, "two": 2, "three": 3, "four": 4,
 	"five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
 }
 
